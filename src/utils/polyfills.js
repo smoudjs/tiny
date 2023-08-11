2f852c74bae3bbb381dd0eb27afb0776754ca61c
Peter Hutsul <peter@greenpandagames.com>
2023-08-11 19:41:44 +0300

    Added Object.assign polyfill

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -1,10 +1,36 @@
 if (!Date.now) {
-  Date.now = function now() {
-    return new Date().getTime();
-  };
+    Date.now = function now() {
+        return new Date().getTime();
+    };
 }
 
-if (typeof Float32Array == "undefined") {
-  window.Float32Array = Array;
-  window.Uint16Array = Array;
+if (typeof Float32Array == 'undefined') {
+    window.Float32Array = Array;
+    window.Uint16Array = Array;
+}
+
+if (Object.assign === undefined) {
+    Object.assign = function (target) {
+        'use strict';
+
+        if (target === undefined || target === null) {
+            throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        var output = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+
+            if (source !== undefined && source !== null) {
+                for (var nextKey in source) {
+                    if (Object.prototype.hasOwnProperty.call(source, nextKey)) {
+                        output[nextKey] = source[nextKey];
+                    }
+                }
+            }
+        }
+
+        return output;
+    };
 }
